52fb9ce64634e521655dfc9e77202502e1ff8840
Michael Ernest <m.hendersun@gmail.com>
2020-08-24 21:46:26 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,5 +2,7 @@
 
 def display_rainbow(color_list)
   puts "-R: #{color[0]}, O: #{color[1]}, Y: #{color[2]}, G: #{color[3]}, B: #{color[4]}, I: #{color[5]}, V: #{color[6]}"
-  
+
 end
+
+display_rainbow
